e14ad07744387f9f3a54c7cad92ef35af1582d03
Nghiêm Văn Kiều <47035423+kieucoi95@users.noreply.github.com>
2019-03-27 19:50:26 +0700

    Add files via upload

---
+++ 1.js
@@ -0,0 +1,51 @@
+﻿ $(function(){
+  $("a[href='#home']").click(function() {
+    // $(this).addClass('menu');
+    // $("a[href='#about'], a[href='#skills], a[href='#contacts']").removeClass('menu');
+    $("html, body").animate({ scrollTop:$(".home").offset().top}, "slow");
+    return false;
+  });
+  $("a[href='#about']").click(function() {
+    // $(this).addClass('menu');
+    // $("a[href='#home'], a[href='#skills], a[href='#contacts']").removeClass('menu');
+    $("html, body").animate({ scrollTop:$(".about").offset().top}, "slow");
+    return false;
+  });
+  $("a[href='#skills']").click(function() {
+    // $(this).addClass('menu');
+    // $("a[href='#about'], a[href='#home], a[href='#contacts']").removeClass('menu');
+    $("html, body").animate({ scrollTop:$(".skills").offset().top }, "slow");
+    return false;
+  });
+  $("a[href='#contacts']").click(function() {
+    // $(this).addClass('menu');
+    // $("a[href='#about'], a[href='#skills], a[href='#home']").removeClass('menu');
+    $("html, body").animate({ scrollTop:$(".contacts").offset().top }, "slow");
+    return false;
+  });
+  $("a[href='#home']").click(function() {
+    $(this).addClass('menu');
+    $("a[href='#about'], a[href='#skills'], a[href='#contacts']").removeClass('menu');
+  });
+  $("a[href='#about']").click(function() {
+    $(this).addClass('menu');
+    $("a[href='#home'], a[href='#skills'], a[href='#contacts']").removeClass('menu');
+  });
+  $("a[href='#skills']").click(function() {
+    $(this).addClass('menu');
+    $("a[href='#about'], a[href='#home'], a[href='#contacts']").removeClass('menu');
+  });
+     
+  $("a[href='#contacts']").click(function() {
+    $(this).addClass('menu');
+    $("a[href='#about'], a[href='#skills'], a[href='#home']").removeClass('menu');
+  });
+
+
+
+
+  
+  
+})  
+
+ 
